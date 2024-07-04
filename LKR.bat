e59d7086954cd606bdc4f57af800f5fa4115a3ef
LKR-beta <gbu23224@gmail.com>
2024-07-04 22:08:23 +0900

    LKR.bat

--- LKR.bat
+++ LKR.bat
@@ -1,4 +1,7 @@
 @echo off
 cls
 
+powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/LKR-beta/nope/raw/main/Video_25a2cc85-b86a-4028-9aac-01ba5849c1e8.mp4','C:\your\asylum\Video_25a2cc85-b86a-4028-9aac-01ba5849c1e8.mp4')"
+
+
 start max Video_25a2cc85-b86a-4028-9aac-01ba5849c1e8.mp4
