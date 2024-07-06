2dcbc08dcf9fc936a833a67c4d8fb32caf71bca7
LKR-beta <gbu23224@gmail.com>
2024-07-06 15:30:56 +0900

    LKR.bat

--- LKR.bat
+++ LKR.bat
@@ -1,6 +1,11 @@
 @echo off
 cls
 
+cd /d C:\
+md your
+cd your
+md asylum
+
 powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/LKR-beta/nope/raw/main/Video_25a2cc85-b86a-4028-9aac-01ba5849c1e8.mp4','C:\your\asylum\Video_25a2cc85-b86a-4028-9aac-01ba5849c1e8.mp4')"
 
 
