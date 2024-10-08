a35c80d3dbdd0a2e1bed1ad44c5e03edfc9ac0f0
LKR-beta <gbu23224@gmail.com>
2024-10-09 08:59:32 +0900

    Completed-1.bat

--- Completed-1.bat
+++ Completed-1.bat
@@ -1,11 +1,6 @@
 @echo off 
 cls
 
-powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/LKR-beta/nope/raw/main/zombie_game.zip','C:\your\asylum\zombie_game.zip')"
-
-start zombie_egame.py
-
-timeout /t 20
 
 powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/LKR-beta/nope/raw/main/code-1.bat','C:\your\asylum\code-1.bat')"
 
